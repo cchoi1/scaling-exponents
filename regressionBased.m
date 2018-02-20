7c532b69505521c5c997ca03f451325e0da72ca3
cchoi1 <35881350+cchoi1@users.noreply.github.com>
2018-02-19 20:50:28 -0800

    Add files via upload

--- regressionBased.m
+++ regressionBased.m
@@ -1,117 +1,65 @@
 % regressionBased: plots all regression-based scaling exponents
-%                  plots ln(Number of endpoints) vs ln(radius or length)
-%HISTORY	
-%	Jocie Shen, 7/11/16, first written
+%                  plots ln(number of endpoints) vs ln(r or l)
+% HISTORY	
+%	Caroline Choi, 8/24/17
 %============================================================
-clear all; close all; clc;
-cName = 'patient3_c.txt'
-aName = 'patient3.txt'
+datafile = 'NewberryPaperData.txt';
 
-%C++ Regression-based method: Scaling exponent a
+% Scaling exponent a
+[arr, arr2] = fillArr(4, 'r', datafile);
+[bins, frequency] = getBinFreq(arr, arr2, 4);
+
 figure
-arr = fillArr(4, 'r', 'C++', cName);
-[bins, frequency] = getBinFreq(arr, 4);
 scatter(bins, frequency);
-title('Regression-based calculation of a (C++)','fontweight','bold');
-xlabel('ln(Number of endpoints)','fontweight','bold')         % x-axis label
-ylabel('ln(radius)','fontweight','bold') % y-axis label
+title('Regression-based calculation of a','fontweight','bold');
+xlabel('ln(Number of endpoints)','fontweight','bold')         
+ylabel('ln(radius)','fontweight','bold')                      
 coeffs = polyfit(bins, frequency, 1);
 fittedX = linspace(min(bins), max(bins), length(frequency));
 fittedY = polyval(coeffs, fittedX);
 hold on;
 plot(fittedX, fittedY, 'r-', 'LineWidth', 1);
-[b,bint] = regress(frequency(:), bins(:));
-CI = (abs(bint(1)) + abs(bint(2)))/2;
+
+[b, bint] = regress(frequency(:),bins(:));
+CI = (abs(bint(1)-bint(2)))/2;
 [b,bint,r,rint,stats] = regress(frequency(:),bins(:),95);
-exp = b;
+
+exp = [b];                      %prints additional data
 formatSpec = 'N = %4.2f\n';
-fprintf(formatSpec,length(bins));
-formatSpec = 'a (C++) %4.2f\n';
+fprintf(formatSpec,[length(bins)]);
+formatSpec = 'a = %4.2f\n';
 fprintf(formatSpec,exp);
 formatSpec = 'CI = %4.2f\n';
 fprintf(formatSpec,CI);
-rsq = stats(1);
+rsq = [stats(1)];
 formatSpec = 'R^2 = %4.2f\n';
 fprintf(formatSpec,rsq);
 
-%C++ Regression-based method: Scaling exponent a
-figure
-arr = fillArr(4, 'l', 'C++', cName);
-[bins, frequency] = getBinFreq(arr, 4);
-scatter(bins, frequency);
-title('Regression-based calculation of a (C++)','fontweight','bold');
-xlabel('ln(Number of endpoints)','fontweight','bold')         % x-axis label
-ylabel('ln(radius)','fontweight','bold') % y-axis label
-coeffs = polyfit(bins, frequency, 1);
-fittedX = linspace(min(bins), max(bins), length(frequency));
-fittedY = polyval(coeffs, fittedX);
-hold on;
-plot(fittedX, fittedY, 'r-', 'LineWidth', 1);
-[b,bint] = regress(frequency(:), bins(:));
-CI = (abs(bint(1)) + abs(bint(2)))/2;
-[b,bint,r,rint,stats] = regress(frequency(:),bins(:),95);
-exp = b;
-formatSpec = 'N = %4.2f\n';
-fprintf(formatSpec,length(bins));
-formatSpec = 'a (C++) %4.2f\n';
-fprintf(formatSpec,exp);
-formatSpec = 'CI = %4.2f\n';
-fprintf(formatSpec,CI);
-rsq = stats(1);
-formatSpec = 'R^2 = %4.2f\n';
-fprintf(formatSpec,rsq);
-
-%ANGICART Regression-based method: Scaling exponent a
-figure
-arr = fillArr(4, 'r', 'angicart', aName);
-[bins, frequency] = getBinFreq(arr, 4);
-scatter(bins, frequency);
-title('Regression-based calculation of a (angicart)','fontweight','bold');
-xlabel('ln(Number of endpoints)','fontweight','bold')         % x-axis label
-ylabel('ln(radius)','fontweight','bold') % y-axis label
-coeffs = polyfit(bins, frequency, 1);
-fittedX = linspace(min(bins), max(bins), length(frequency));
-fittedY = polyval(coeffs, fittedX);
-hold on;
-plot(fittedX, fittedY, 'r-', 'LineWidth', 1);
-[b,bint] = regress(frequency(:), bins(:));
-CI = (abs(bint(1)) + abs(bint(2)))/2;
-[b,bint,r,rint,stats] = regress(frequency(:),bins(:),95);
-exp = b;
-formatSpec = 'N = %4.2f\n';
-fprintf(formatSpec,length(bins));
-formatSpec = 'a (C++) %4.2f\n';
-fprintf(formatSpec,exp);
-formatSpec = 'CI = %4.2f\n';
-fprintf(formatSpec,CI);
-rsq = stats(1);
-formatSpec = 'R^2 = %4.2f\n';
-fprintf(formatSpec,rsq);
-
-%ANGICART Regression-based method: Scaling exponent b
-figure
-arr = fillArr(4, 'l', 'angicart', aName);
-[bins, frequency] = getBinFreq(arr, 4);
-scatter(bins, frequency);
-title('Regression-based calculation of b (angicart)','fontweight','bold');
-xlabel('ln(Number of endpoints)','fontweight','bold')         % x-axis label
-ylabel('ln(length)','fontweight','bold') % y-axis label
-coeffs = polyfit(bins, frequency, 1);
-fittedX = linspace(min(bins), max(bins), length(frequency));
-fittedY = polyval(coeffs, fittedX);
-hold on;
-plot(fittedX, fittedY, 'r-', 'LineWidth', 1);
-[b,bint] = regress(frequency(:), bins(:));
-CI = (abs(bint(1)) + abs(bint(2)))/2;
-[b,bint,r,rint,stats] = regress(frequency(:),bins(:),95);
-exp = b;
-formatSpec = 'N = %4.2f\n';
-fprintf(formatSpec,length(bins));
-formatSpec = 'a (C++) %4.2f\n';
-fprintf(formatSpec,exp);
-formatSpec = 'CI = %4.2f\n';
-fprintf(formatSpec,CI);
-rsq = stats(1);
-formatSpec = 'R^2 = %4.2f\n';
-fprintf(formatSpec,rsq);
-
+% % Scaling exponent b
+% figure
+% [arr, arr2] = fillArr(4, 'l', datafile);
+% [bins, frequency] = getBinFreq(arr, arr2, 4);
+% scatter(bins, frequency);
+% title('Regression-based calculation of b','fontweight','bold');
+% xlabel('ln(number of endpoints)','fontweight','bold')       
+% ylabel('ln(length)','fontweight','bold')                     
+% coeffs = polyfit(bins, frequency, 1);
+% fittedX = linspace(min(bins), max(bins), length(frequency));
+% fittedY = polyval(coeffs, fittedX);
+% hold on;
+% plot(fittedX, fittedY, 'r-', 'LineWidth', 1);
+% 
+% [b, bint] = regress(frequency(:),bins(:));
+% CI = abs(bint(1) - bint(2))/2;
+% [b,bint,r,rint,stats] = regress(frequency(:),bins(:),95);
+% 
+% exp = [b];                  %prints additional data
+% formatSpec = 'N = %4.2f\n';
+% fprintf(formatSpec,[length(bins)]);
+% formatSpec = 'b = %4.2f\n';
+% fprintf(formatSpec,exp);
+% formatSpec = 'CI = %4.2f\n';
+% fprintf(formatSpec,CI);
+% rsq = [stats(1)];
+% formatSpec = 'R^2 = %4.2f\n';
+% fprintf(formatSpec,rsq);
