b1a3c2342cd5382268119c5df8242e3b63e7f9f2
Caroline Choi <35881350+cchoi1@users.noreply.github.com>
2018-06-20 17:00:57 -0700

    Add files via upload
    updated version (4/5/18)

--- calcConserve.m
+++ calcConserve.m
@@ -1,28 +1,23 @@
-function a = calcConserve(arrOfChildrenLengths, parentLength)
-%calcConserve: function that calculates the scaling exponents a and b using the
-%conservation-based method
+function a = calcConserve(arr_children_lengths, parent_length)
+%Calculates the scaling exponents a and b using the
+%conservation-based method.
 %
 %INPUT:
-% rk:    radius or length of the parent vessel
-% rk1:   radius or length of the child vessel
+% arrOfChildrenLengths: array of children radii or lengths
+% parentLength: parent vessel radius or length
 %
 %OUTPUT:
-%  a:   scaling exponent
-%
-%EX:
-% rk = 0.2
-% rk1 = 0.1
-% a = calcConserve(rk, rk1)
+%  a: scaling exponent
 %	
-%	Caroline Choi, 8/23/17
+%Caroline Choi
 %============================================================
 syms x
 sum=0;
-for i=1:length(arrOfChildrenLengths)
-    sum = sum + arrOfChildrenLengths(i)^(x);
+for i=1:length(arr_children_lengths)
+    sum = sum + arr_children_lengths(i)^(x);
 end
-eqn = parentLength^(x) == sum;
-a = solve(eqn,x);
+eqn = parent_length^(x) == sum;
+a = vpasolve(eqn,x);
 a = double(a);
 a = 1/a;
 end
